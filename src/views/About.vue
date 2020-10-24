d3b4e7d62cc32bcaf8ebbfc490debd46b55c78ab
Hazem Gharib <hazem.gharib@servicerocket.com>
2020-10-24 14:00:46 +0800

    Add vuetify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="text-center my-4">
+    <h1>About</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  components: {
+    //
+  }
+};
+</script>
